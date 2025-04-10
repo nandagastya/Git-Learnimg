438b704b16b8e6ae06b32b5f48645305f740fca5
nandagastya <nandagastya25@gmail.com>
2025-04-10 15:53:10 +0530

    new change

--- Git Learnimg/ContentView.swift
+++ Git Learnimg/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Button("Show Modal") {
+                Button("Show Model") {
                     self.isPresent = true
                 }
                 .sheet(isPresented: $isPresent) {
